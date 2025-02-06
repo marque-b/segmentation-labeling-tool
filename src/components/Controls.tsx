75143dcf64330f6f82cc598ad08a07022bb5d79d
Bruno Marques <100382974+marque-b@users.noreply.github.com>
2025-02-06 19:19:08 -0300

    feat(controls): Add navigate back button

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -1,7 +1,7 @@
 import { useCOCOStore } from "@/store/useCOCOStore";
 import { ThemeToggle } from "./ThemeToggle";
 import { Button } from "./ui/button";
-import { House } from "lucide-react";
+import { House, ChevronLeft } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 
 function Controls() {
@@ -11,6 +11,9 @@ function Controls() {
   return (
     <div className="container">
       <nav className="flex justify-end items-center min-h-14 gap-2">
+        <Button variant="outline" onClick={() => navigate(-1)}>
+          <ChevronLeft />
+        </Button>
         <Button variant="outline" onClick={() => navigate("/")}>
           <House />
         </Button>
