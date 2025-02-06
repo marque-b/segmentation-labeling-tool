d3ab43a6b606f6fc9245c67bebcf8fda09577cb2
Bruno Marques <100382974+marque-b@users.noreply.github.com>
2025-02-06 11:20:29 -0300

    feat(shadcn): Install shadcn

--- src/components/Controls.tsx
+++
@@ -1,5 +0,0 @@
-function Controls() {
-  return <div className="w-full bg-black">Controls</div>;
-}
-
-export default Controls;
--- src/components/ImageCanvas.tsx
+++
@@ -1,5 +0,0 @@
-function ImageCanvas() {
-  return <div>ImageCanvas</div>;
-}
-
-export default ImageCanvas;
